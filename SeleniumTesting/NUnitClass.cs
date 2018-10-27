11c123678c76399f93ea6e9100fbb9ae77c97c70
krish-nair <krish.nair101@gmail.com>
2018-10-27 18:23:28 +1300

    Running the 1st test case for navigation and maximise window

---
+++ SeleniumTesting/NUnitClass.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTesting
+{
+    class NUnitClass
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Url = "https://www.google.com/";
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
